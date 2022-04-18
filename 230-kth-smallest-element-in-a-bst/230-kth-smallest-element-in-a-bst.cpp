d9def3bb284fd2dd0f6075e9fedf0f90aa7ebf25
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-04-18 06:02:38 +0530

    Time: 19 ms (79.16%), Space: 24.5 MB (16.05%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -26,18 +26,3 @@ public:
         return x;
     }
 };
-
-// int cnt=0,ans=-1;
-//     void traverse(TreeNode* root,int k){
-//         if(root==NULL)  return;     //if recahes NULL return
-//         if(ans!=-1)  return;        //once we will get the ans than no need to traverse further
-//         traverse(root->left,k);
-//         cnt++;
-//         if(cnt==k){
-//             ans=root->val;  return; 
-//         }
-//         traverse(root->right,k);
-//     }
-//     int kthSmallest(TreeNode* root, int k) {
-//         traverse(root,k);
-//         return ans;
