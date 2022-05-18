0e1ef79ece26c9487f5c346f6a229cc96af7e707
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-05-18 06:50:06 +0530

    Time: 40 ms (21.85%), Space: 16.8 MB (86.84%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
+        //XOR OF SAME NUMBERS IS ZERO 
+        //WE DO XOR OF ALL NUMBER AND SAME NUMBERS BECAME ZERO AND ONLY SINGLE ONE IS LEFT.
 	    for(auto x:nums)
-	    ans^=x;
+	        ans^=x;
 	    return ans;
     }
 };
