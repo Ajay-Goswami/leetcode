19668ad1946f77320407f8128b1bf2db697e813c
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-05-18 06:56:00 +0530

    Time: 27 ms (47.05%), Space: 19.6 MB (14.32%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //MAP IS USED FOR STORE VALUE AND ITS OCCURANCE
         map<int, int> m;
+        //L IS USED FOR CHECKING MAJORITY ELEMENT OCCURANCE
         int L = nums.size()/2;
+        
         for(int i=0; i<nums.size(); i++){
              m[nums[i]]++;
+            //IF ANY ELEMENT OCCURANCE IS GREATER THAN L THAN RETURN IT
              if(m[nums[i]] > L)
-             return nums[i];
+                return nums[i];
         }
+        //IF NO ELEMENT OCCURNACE IS GREATER THAN L RETURN EMPTY 
         return {};
     }
 };
