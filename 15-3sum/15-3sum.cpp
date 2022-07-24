4bd1fed8b0d64de590d2a38a3b2654c4eb0ed4cc
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-07-24 12:59:21 +0530

    Time: 143 ms (53.93%), Space: 19.9 MB (95.74%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,43 +1,31 @@
 class Solution {
-    vector<vector<int>> res;
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // if((nums.size() == 0) || (nums.size() == 1))
-        //     return {};
+        vector<vector<int> > result;
+        int sum, n = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++){
-            //check for duplicate a
-            if( i==0 || nums [i-1]!=nums[i]){
-                twoSum(i+1,nums.size()-1, nums, 0-nums[i]);         //a+b+c=0  => b+c=0-a
+        for(int i=0; i<n-2; i++){
+            if(i==0 || nums[i] != nums[i-1]){
+                int left = i+1;
+                int right = n-1;
+                while(left < right){
+                    sum = nums[i] + nums[left] + nums[right];
+                    if(sum == 0){
+                        result.push_back({nums[i], nums[left], nums[right]});
+                        while(left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while(left < right && nums[right] == nums[right - 1])
+                            right--;
+                        left++;
+                        right--;
+                    }
+                    else if(sum < 0)
+                        left++;
+                    else
+                        right--;
+                }
             }
         }
-        return res;
-    }
-    
-    void twoSum(int i, int j, vector<int> nums, int target){
-        int a1 = nums[i-1];
-        while(i<j){
-            if(nums[i]+nums[j]>target)
-                j--;
-            else if(nums[i]+nums[j]<target)
-                i++;
-            else{
-                vector<int> v;
-                v.push_back(a1);
-                v.push_back(nums[i]);
-                v.push_back(nums[j]);
-                
-                res.push_back(v);
-                
-                // check for duplicate b
-                while(i<j && nums[i]==nums[i+1])
-                    i++;
-                //check for duplicate c
-                while(i<j && nums[j]==nums[j-1])
-                    j--;
-                
-                i++,j--;
-            }
-        }
+        return result;
     }
 };
