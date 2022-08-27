bb1a1fc7af7792fddeb42ef5a1a4a4c28648f7d7
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-08-27 12:48:12 +0530

    Time: 2 ms (57.70%), Space: 6.6 MB (40.23%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,20 +1,35 @@
 class Solution {
-public:
-    void solve(int st,vector<vector<int>>& ans,vector<int>& temp,int k,int n){
-        if(n==0 and temp.size()==k){ 
-            ans.push_back(temp);
+    void help(int i, int k, int sumTillNow, int n, vector<int> &subSet, vector<vector<int>> &ans){
+        if(sumTillNow > n)
+            return ;
+    
+        if(k == 0){
+                if(sumTillNow == n){
+                    ans.push_back(subSet);
+                }
             return;
         }
-        for(int i=st;i<=9;i++){
-            temp.push_back(i);
-            solve(i+1,ans,temp,k,n-i);
-            temp.pop_back();
-        }
-    }
+    
+        if(i == 10)
+            return ;
+    
+        sumTillNow += i ;
+        subSet.push_back(i) ;
+    
+        help(i + 1, k - 1, sumTillNow, n, subSet, ans) ;
+    
+        sumTillNow -= i ;
+        subSet.pop_back() ;
+
+        help(i + 1, k, sumTillNow, n, subSet, ans);
+}
+
+public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> temp;
-        vector<vector<int>> ans;
-        solve(1,ans,temp,k,n);
-        return ans;
+        vector<int> subSet ;
+        vector<vector<int>> ans ;
+        help(1, k, 0, n, subSet, ans) ;
+        return ans ;
     }
 };
+
