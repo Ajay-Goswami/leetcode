af80c993b3716c5530f8e01717a7daf4928abce1
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2022-09-11 13:19:24 +0530

    Time: 5 ms (14.16%), Space: 5.7 MB (88.83%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,21 +4,22 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int low=1;
-        int high=n;
-        
+        int l=1, h=n;
         int firstBadVersion=-1;
-        
-        while(low<=high){
-            int mid= low +(high-low)/2;
-            
-            if(isBadVersion(mid)==true){
+        int mid;
+        while(l<=h){
+            mid=l+(h-l)/2;
+            if(isBadVersion(mid)){
                 firstBadVersion=mid;
-                high=mid-1;
+                h=mid-1;   
             }
-            else
-                low=mid+1;
+            else {
+                l=mid+1;
+            }
         }
         return firstBadVersion;
     }
 };
+
+
+        
