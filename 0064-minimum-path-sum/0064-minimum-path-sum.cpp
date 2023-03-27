b8fc934b0e4227678ad1e2f5eebd4592392be739
Ajay Goswami <90101997+Ajay-Goswami@users.noreply.github.com>
2023-03-27 21:24:09 +0530

    Time: 13 ms (29.88%), Space: 9.9 MB (76.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
+private:
+    int dp[201][201];
+    
+    int rec(vector<vector<int>>& grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+            return 1e9;
+        
+        if(i == grid.size() - 1 && j == grid[0].size() - 1)
+            return grid[i][j];
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        return dp[i][j] = min(
+            grid[i][j] + rec(grid, i + 1, j), 
+            grid[i][j] + rec(grid, i, j + 1)
+        );
+    }
+
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        if(rows==0)
-            return 0;
-        int cols = grid[0].size();
-        vector<vector<int>> dp(rows,vector<int>(cols,0));
-        int i,j;
+        memset(dp, -1, sizeof dp);
         
-        dp[0][0] = grid[0][0];  
-        //Fill 1st row
-        for(i=1;i<cols;++i)
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        
-        //Fill 1st Col
-        for(i=1;i<rows;++i)
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        
-        //Now fill the rest of the cell
-        for(i=1;i<rows;++i){
-            for(j=1;j<cols;++j)
-                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
-        }
-        return dp[rows-1][cols-1];
+        return rec(grid, 0, 0);
     }
 };
